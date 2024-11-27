05ec1e0e02aca41de2a8d811081be9e9a88b8f82
laminthurein1411 <78501447+laminthurein1411@users.noreply.github.com>
2024-11-27 18:20:50 +0630

    Update spotify-backup.py

--- spotify-backup.py
+++ spotify-backup.py
@@ -51,29 +51,6 @@ logging.basicConfig(level=20,
                     format="[%(asctime)s] %(message)s")
 
 
-# simple recursive y/n input with default
-def yesno(question, default=True):
-    ans = input(question).strip().lower()
-
-    if default is not None:
-        if ans == '':
-            if default == 'y':
-                return True
-            return False
-        elif ans not in ['y', 'n']:
-            print(f'{ans} is invalid, please try again...')
-            return yesno(question)
-        if ans == 'y':
-            return True
-    else:
-        if ans not in ['y', 'n']:
-            print(f'{ans} is invalid, please try again...')
-            return yesno(question)
-        if ans == 'y':
-            return True
-
-    return False
-
 
 # return formatted hh mm ss
 def timematter(x):
